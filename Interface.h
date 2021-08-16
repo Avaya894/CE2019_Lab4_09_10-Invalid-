71776ec62ee2b8c0afa719aca747c466b29b5d98
Avaya894 <59593349+Avaya894@users.noreply.github.com>
2021-08-16 14:00:51 +0545

    Corrected Interface class

--- Interface.h
+++ Interface.h
@@ -2,23 +2,25 @@
 #ifndef G_INTERFACE
 #define G_INTERFACE
 
+#include <iostream>
+
 class InterfaceGraph
 {
 public:
     virtual ~InterfaceGraph() { }
     virtual bool isEmpty() = 0;
-    virutal bool isDirected() = 0;
-    virutal void addVertex(char v) = 0;
+    virtual bool isDirected() = 0;
+    virtual void addVertex(char v) = 0;
     virtual void addEdge(char v1, char v2) = 0;
     virtual void removeVertex(char v) = 0;
     virtual void removeEdge(v1, v2) = 0;
-    virutal int numVertices() = 0;
+    virtual int numVertices() = 0;
     virtual int numEdges() = 0;
-    virutal int indegree(char v) = 0;
-    virutal int outdegree(char v) = 0;
+    virtual int indegree(char v) = 0;
+    virtual int outdegree(char v) = 0;
     virtual int degree(char v) = 0;
     virtual std::string neighbours(char v) = 0;
-    virutal bool neighbour(char v1, char v2) = 0;
+    virtual bool neighbour(char v1, char v2) = 0;
 
 };
 
